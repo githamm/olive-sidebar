de6c69fb992e83d50c0bf93b5fd3c88e896c26d7
githamm <hamm.kevin@gmail.com>
2015-05-06 15:44:19 -0600

    Page build

--- OliveAppInst.js
+++
@@ -1,5 +0,0 @@
-
-function OwcGetApplication()
-{return OwcGetApplication.Application;}
-var applicationClass=Olive.WebAppBase.ApplicationClass;if(applicationClass==null||applicationClass==undefined)
-applicationClass=OliveApplication;OwcGetApplication.Application=new applicationClass();OwcGetApplication.Application.initialize();
